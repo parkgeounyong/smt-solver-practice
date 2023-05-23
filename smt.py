827f901da023716a52962038975004b1d3c0acb1
parkgeounyong <qkrnsdyd1@gmail.com>
2023-05-23 11:04:19 +0900

    update

--- smt.py
+++ smt.py
@@ -1,40 +1,29 @@
 from z3 import *
 
-# 버블 정렬을 나타내는 불린 변수를 생성.
-bubbleSort = Bool('bubbleSort')
-
+# 시간 복잡도를 나타내는 정수 변수 생성
 n = Int('n')
 
-# 시간 복잡도가 n^2이라는 불린 변수를 생성.
-timeComplexN2 = Bool('timeComplexN2')
-
-
-# SMT Solver를 생성.
+# 불린 변수 생성
+bubbleSort = Bool('bubbleSort')
+mergeSort = Bool('mergeSort')
+timecomplex= Int('timecomplex')
+# SMT Solver 생성
 s = Solver()
 
+# 버블 정렬과 병합 정렬의 조건 추가
+s.add(Implies(timecomplex==n*n, bubbleSort))
+s.add(Implies(timecomplex==n, mergeSort))
 
-# Sygus에서 나온 것들을 add 할 때 각 원소로 추가하면 된다.
-# smt에는 조건식이 이미 추가되어 있다.
-# Sygus에서 나온 원소들이 조건식을 만족할 
-# 버블 정렬의 조건을 추가.
-s.add(bubbleSort)
-
-s.add(timeComplexN2)
-
-s.add(timeComplexN2 == (n * n))
-
-
-# 버블 정렬이 n^2 시간 복잡도를 가지는 조건을 추가.
-s.add(Implies(bubbleSort, And(timeComplexN2, timeComplexN2 == (n * n)))) #오리면 만족
-
-
-
-# 이 문제가 만족 가능한지(SAT) 확인하고 결과를 출력.
+#시간 복잡도 추가
+s.add(timecomplex==n*n)
+s.add(n>1)
+# 이 문제가 만족 가능한지(SAT) 확인하고 결과 출력
 result = s.check()
 if result == sat:
     print("검증 결과: SAT")
-    print("이 조건은 버블 정렬을 나타냅니다.")
-    print(s.model())
+    model = s.model()
+    print(model)
+
 else:
     print("검증 결과: UNSAT")
-    print("이 조건은 버블 정렬을 나타내지 않습니다.")
+    print("주어진 시간 복잡도에 해당하는 정렬을 구분할 수 없습니다.")
